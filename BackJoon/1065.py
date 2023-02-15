33d548eabe63c972f20766271ab92b99562347ef
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-02-15 21:28:34 +0900

    한수

---
+++ BackJoon/1065.py
@@ -0,0 +1,11 @@
+import sys
+
+n= int(sys.stdin.readline())
+answer = 0
+for i in range(1, n+1):
+    num_list =list(map(int, str(i)))
+    if i < 100:
+        answer +=1
+    elif num_list[1] - num_list[0] == num_list[2] - num_list[1]:
+        answer +=1
+print(answer)

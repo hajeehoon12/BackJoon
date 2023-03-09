7966d577d5ac31cc15fa5d683f305c5a8497a009
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-03-09 19:28:33 +0900

    GCD

---
+++ BackJoon/1934.py
@@ -0,0 +1,11 @@
+import sys
+n= int(sys.stdin.readline())
+
+for i in range(n):
+    a,b = map(int, sys.stdin.readline().split().strip())
+    a1,b1 = a,b
+
+    while a%b !=0:
+        a,b=b,a%b
+
+    print(a1*b1//b)

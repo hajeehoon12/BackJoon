0d2ddbcc72da6a946833b0ed48d8029ecf0a8a58
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-03-10 17:21:06 +0900

    set for time complex

---
+++ BackJoon/14425.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+s= set()
+for _ in range(N):
+    s.add(input())
+
+cnt = 0
+for _ in range(M):
+    t = input()
+    if t in s:
+        cnt += 1
+print(cnt)

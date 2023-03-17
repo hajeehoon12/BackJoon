4e7f6b017fa8738f1b010eb385c18950aa47d211
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-03-17 12:33:11 +0900

    remove recurse

--- BackJoon/15650.py
+++ BackJoon/15650.py
@@ -1,13 +1,13 @@
 n,m = list(map(int,input().split()))
 s = []
-def dfs():
+def dfs(start):
     if len(s)==m:
         print(' '.join(map(str,s)))
         return
     
-    for i in range(1,n+1):
+    for i in range(start,n+1):
         if i not in s:
             s.append(i)
-            dfs()
+            dfs(i+1)
             s.pop()
-dfs()
+dfs(1)

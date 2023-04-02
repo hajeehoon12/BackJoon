1658afb49891c6c6e4ff38d3e3b12b3a510a101c
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-04-02 12:51:21 +0900

    permutation

---
+++ BackJoon/1010.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    ans = factorial(m) / (factorial(n) * factorial(m - n))
+    print(int(ans))

04937b3563497fdfa8f710988139872551e97a2b
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-10-27 16:06:00 +0900

    d

--- BackJoon/1010.py
+++ BackJoon/1010.py
@@ -12,5 +12,5 @@ t = int(sys.stdin.readline())
 
 for _ in range(t):
     n, m = map(int, sys.stdin.readline().split())
-    ans = factorial(m) / (factorial(n) * factorial(m - n))
-    print(int(ans))
+    ans = factorial(m) // (factorial(n) * factorial(m - n))
+    print(ans)

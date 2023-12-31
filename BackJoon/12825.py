6b8684f4fb33afc26f21e94f9c5d819c567b827e
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2023-12-31 15:40:12 +0900

    dp reversed

---
+++ BackJoon/12825.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+dp = [[0, []] for _ in range(n+1)]
+
+dp[1][1] = [1]
+#print(dp)
+
+for i in range(2, n+1):
+
+    dp[i][0] = dp[i-1][0] + 1
+    dp[i][1] = dp[i-1][1] + [i]
+
+    if i % 3 ==0 and dp[i//3][0] + 1 < dp[i][0]:
+        dp[i][0] = dp[i//3][0] + 1
+        dp[i][1] = dp[i//3][1] + [i]
+        
+    if i % 2 == 0 and dp[i//2][0]+1 < dp[i][0]:
+        dp[i][0] = dp[i//2][0] + 1
+        dp[i][1] = dp[i//2][1] + [i]
+    
+print(dp[n][0])
+#print(dp)
+print(*reversed(dp[n][1])) 
+    
